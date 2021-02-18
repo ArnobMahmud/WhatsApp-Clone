bad97418ae29bd3e76aa70ab1f0169a189b88162
ArnobMahmud <arnob.tech.me@gmail.com>
2021-02-18 17:15:25 +0600

    models implemented + chat section completed

---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class User {
+  User({
+    this.id,
+    this.name,
+    this.imageUrl,
+  });
+
+  final int id;
+  final String imageUrl;
+  final String name;
+}
--- lib/pages/chat-page.dart
+++ lib/pages/chat-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/message_model.dart';
 
 class ChatPage extends StatefulWidget {
   ChatPage({Key key}) : super(key: key);
@@ -15,6 +16,89 @@ class _ChatPageState extends State<ChatPage> {
         child: Icon(Icons.chat),
         onPressed: null,
       ),
+      body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: chats.length,
+          itemBuilder: (BuildContext context, int index) {
+            final Message chat = chats[index];
+            return Container(
+              margin: EdgeInsets.only(right: 20.0, top: 5.0, bottom: 5.0),
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              decoration: BoxDecoration(
+                  color: chat.unread ? Color(0xFFFFEFEE) : Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30.0,
+                        backgroundImage: AssetImage(chat.sender.imageUrl),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            chat.sender.name,
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 6.0,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.45,
+                            child: Text(
+                              chat.text,
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        chat.time,
+                        style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                        height: 20.0,
+                        width: 40.0,
+                        decoration: BoxDecoration(
+                            color: chat.unread ? Colors.red : Colors.white,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Text(
+                          'NEW',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        alignment: Alignment.center,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          }),
     );
   }
 }
